53c8131cbaf496ff66f98e6012c74d9e0fabfab0
SP <sapak@protonmail.com>
2022-10-30 14:26:01 +0200

    refactor

--- app.js
+++ app.js
@@ -97,7 +97,9 @@ const errorHandler = (error, request, response, next) => {
 }
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, () => {
+// 	console.log(`Server running on port ${PORT}`)
+// })
+
+module.exports = app
