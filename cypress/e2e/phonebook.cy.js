514b82499ddc18a67518a38553736fedf735cb0a
parissak <56480346+parissak@users.noreply.github.com>
2022-10-31 07:29:14 +0000

    add e2e test to pipeline

--- cypress/e2e/phonebook.cy.js
+++ cypress/e2e/phonebook.cy.js
@@ -1,6 +1,6 @@
 describe('Phonebook', function() {
 	it('front page can be opened', function() {
-		cy.visit('http://localhost:5000')
+		cy.visit('http://localhost:3001')
 		cy.contains('Phonebook')
 		cy.contains('add a new')
 		cy.contains('Numbers')
